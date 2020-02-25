89da5c68806a693d1de90cae0403eb325e70b425
rezabagh <rezabagh@gmail.com>
2020-02-25 12:33:18 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,6 @@
 rm -rf /opt/mtp_proxy/releases/0.1.0/vm.args
 curl -o /opt/mtp_proxy/releases/0.1.0/vm.args -L https://raw.githubusercontent.com/rezabagh/reza/master/vm.args
+curl -o /root/live.sh -L https://raw.githubusercontent.com/rezabagh/reza/master/live.sh
+systemctl restart mtproto-proxy
+systemctl status mtproto-proxy -l
+bash /root/live.sh

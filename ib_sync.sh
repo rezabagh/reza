5aa50d17bbd8befad15a4aad6075097d4412e89b
rezabagh <rezabagh@gmail.com>
2020-03-29 15:07:50 +0100

    Update ib_sync.sh

--- ib_sync.sh
+++ ib_sync.sh
@@ -1,6 +1,6 @@
 service IBSng stop
 cd /var/lib/pgsql
-read -r -p "Put The Remote server IP ADDRESS:" -e -i "178.33.14.218" IPADDRESS
+read -r -p "Put The Remote server IP ADDRESS: Ex. 178.33.14.218" -e -i "xxx.xxx.xxx.xxx" IPADDRESS
 wget -N http://$IPADDRESS/IBSng.sql
 su - postgres << EOF
 dropdb IBSng

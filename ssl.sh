7f1bd6bab8931d39e9fb1807d951de14a8b9bd68
rezabagh <rezabagh@gmail.com>
2020-05-23 12:08:14 +0100

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,1 +1,3 @@
-
+read -r -p "Enter Domain:" -e -i "www.example.com" DOMAIN
+certbot certonly --manual --preferred-challenges=http --manual-public-ip-logging-ok -d $DOMAIN
+echo $CERTBOT_VALIDATION > /etc/letsencrypt/validation/$CERTBOT_TOKEN

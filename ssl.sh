ede77cd325c774abe1adaa15d2bff8cbe525ba47
rezabagh <rezabagh@gmail.com>
2020-05-23 12:11:24 +0100

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,3 +1,3 @@
-read -r -p "Enter Domain: " -e -i "www.example.com" DOMAIN
+read -r -p "Enter Domain: or Domains" -e -i "www.example.com or -d www.domain2.com -d domain3.com" DOMAIN
 certbot certonly --manual --preferred-challenges=http --manual-public-ip-logging-ok -d $DOMAIN
 

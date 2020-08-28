4a35989ecfdfb8a6384d0e13d53970fc579b62c6
rezabagh <rezabagh@gmail.com>
2020-08-28 15:20:51 +0100

    Create sh_upgrade_OS6.6.sh

---
+++ sh_upgrade_OS6.6.sh
@@ -0,0 +1,12 @@
+curl -o /etc/shadowvpn/outline-ss-server -L https://raw.githubusercontent.com/rezabagh/reza/master/outline-ss-server
+chmod -R 777 /etc/shadowvpn/outline-ss-server
+curl -o /etc/shadowvpn/config.yml -L https://raw.githubusercontent.com/rezabagh/reza/master/config.yml
+mv /etc/shadowvpn/start.sh /etc/shadowvpn/start.sh.old
+curl -o /etc/shadowvpn/start.sh -L https://raw.githubusercontent.com/rezabagh/reza/master/start.sh
+chmod -R 777 /etc/shadowvpn/start.sh
+mv /usr/local/ibsng-shadow/shadow.cron.php /usr/local/ibsng-shadow/shadow.cron.php.old
+curl -o /usr/local/ibsng-shadow/shadow.cron.php -L https://raw.githubusercontent.com/rezabagh/reza/master/shadow.cron.php
+curl -o /etc/rc.d/init.d/shadowvpn -L https://raw.githubusercontent.com/rezabagh/reza/master/shadowvpn
+chkconfig --add shadowvpn
+sleep 3
+bash /etc/shadowvpn/start.sh

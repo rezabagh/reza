a6507ffa045279109c397300aa4647acc1d8e0bd
rezabagh <rezabagh@gmail.com>
2020-08-28 15:09:40 +0100

    Update sh_upgrade_OS7.sh

--- sh_upgrade_OS7.sh
+++ sh_upgrade_OS7.sh
@@ -4,6 +4,7 @@ chmod -R 777 /etc/shadowvpn/outline-ss-server
 curl -o /etc/shadowvpn/config.yml -L https://raw.githubusercontent.com/rezabagh/reza/master/config.yml
 mv /etc/shadowvpn/start.sh /etc/shadowvpn/start.sh.old
 curl -o /etc/shadowvpn/start.sh -L https://raw.githubusercontent.com/rezabagh/reza/master/start-OS7.sh
+chmod -R 777 /etc/shadowvpn/start.sh
 mv /usr/local/ibsng-shadow/shadow.cron.php /usr/local/ibsng-shadow/shadow.cron.php.old
 curl -o /usr/local/ibsng-shadow/shadow.cron.php -L https://raw.githubusercontent.com/rezabagh/reza/master/shadow.cron.php
 curl -o /etc/systemd/system/shadowsocksvpn.service -L https://raw.githubusercontent.com/rezabagh/reza/master/shadowsocksvpn.service

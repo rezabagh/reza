4ccf07bf98f39d1aa3554ce8102974d249ed8b32
rezabagh <rezabagh@gmail.com>
2020-09-17 12:35:17 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+PORT = 443
+USERS = {"vpsi": "9cf0176721eddc8bb50878cfc1e4b707"}
+USER_MAX_TCP_CONNS = {  }
+TLS_DOMAIN = "www.cloudflare.com"
+MODES = { "classic": False, "secure": False, "tls": True }
+

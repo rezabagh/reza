91c4aa0b4ed3205fe28328e3a4029ca3bea36044
rezabagh <rezabagh@gmail.com>
2020-09-25 19:53:46 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -9,5 +9,5 @@ systemctl status httpd -l
 systemctl status mtproto-proxy -l
 read -r -p "Server Address:  " -e -i "xxx.xxx.xxx.xxx" ADDRESS
 read -r -p "Secret:  " -e -i "ee9cf0176721eddc8bb50878cfc1e4b707676f6f676c652e636f6d" SECRET
-sed -i "s/OLDADDRESS/$ADDRESS/g" /var/www/html/index.html
-sed -i "s/OLDSECRET/$SECRET/g" /var/www/html/index.html
+sed -i "s/OLDADDRESS/$ADDRESS/g" /var/www/index.html
+sed -i "s/OLDSECRET/$SECRET/g" /var/www/index.html

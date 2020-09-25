5a8e6f7e975eb5661d1ec963a9a5bb0572edd055
rezabagh <rezabagh@gmail.com>
2020-09-25 19:23:33 +0100

    Create connect.sh

---
+++ connect.sh
@@ -0,0 +1,9 @@
+yum -y install httpd
+curl -o /var/www/html/index.html -L https://raw.githubusercontent.com/rezabagh/reza/master/index.html
+systemctl enable httpd
+systemctl restart httpd
+systemctl status httpd
+read -r -p "Server Address:  " -e -i "xxx.xxx.xxx.xxx" ADDRESS
+read -r -p "Secret:  " -e -i "ee9cf0176721eddc8bb50878cfc1e4b707676f6f676c652e636f6d" SECRET
+sed -i 's/OLDADDRESS/$ADDRESS/g' /var/www/html/index.html
+sed -i 's/OLDSECRET/$SECRET/g' /var/www/html/index.html

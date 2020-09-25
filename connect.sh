0434d3f9cab5ddc27b2f0c61803bd671e7687fa0
rezabagh <rezabagh@gmail.com>
2020-09-25 19:32:59 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -2,7 +2,7 @@ yum -y install httpd
 curl -o /var/www/html/index.html -L https://raw.githubusercontent.com/rezabagh/reza/master/index.html
 systemctl enable httpd
 systemctl restart httpd
-systemctl status httpd
+systemctl status httpd -l
 systemctl status mtproto-proxy -l
 read -r -p "Server Address:  " -e -i "xxx.xxx.xxx.xxx" ADDRESS
 read -r -p "Secret:  " -e -i "ee9cf0176721eddc8bb50878cfc1e4b707676f6f676c652e636f6d" SECRET

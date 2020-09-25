c0b9dba99d96a02a8f40193da2fd3bd491b19696
rezabagh <rezabagh@gmail.com>
2020-09-25 19:31:24 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -3,6 +3,7 @@ curl -o /var/www/html/index.html -L https://raw.githubusercontent.com/rezabagh/r
 systemctl enable httpd
 systemctl restart httpd
 systemctl status httpd
+systemctl status mtproto-proxy -l
 read -r -p "Server Address:  " -e -i "xxx.xxx.xxx.xxx" ADDRESS
 read -r -p "Secret:  " -e -i "ee9cf0176721eddc8bb50878cfc1e4b707676f6f676c652e636f6d" SECRET
 sed -i 's/OLDADDRESS/$ADDRESS/g' /var/www/html/index.html

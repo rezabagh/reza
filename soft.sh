f2cac73a1175f03770b44c306449a674cbea9309
rezabagh <rezabagh@gmail.com>
2020-09-27 10:52:21 +0100

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -14,6 +14,7 @@ chmod 755 /etc/init.d/vpnserver
 systemctl daemon-reload
 mkdir /var/lock/subsys
 chkconfig --add vpnserver
+/sbin/chkconfig vpnserver on
 /etc/init.d/vpnserver start
 systemctl enable vpnserver
 systemctl restart vpnserver

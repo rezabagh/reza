04ffaf79f620c7652deb026abc7f3cddcb1d792e
rezabagh <rezabagh@gmail.com>
2020-09-27 10:26:26 +0100

    Create soft.sh

---
+++ soft.sh
@@ -0,0 +1,18 @@
+yum groupinstall "Development Tools"
+wget https://www.softether-download.com/files/softether/v4.34-9745-rtm-2020.04.05-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz
+tar -xvzf softether-vpnserver-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz
+cd vpnserver
+make
+cd ..
+mv vpnserver /usr/local
+cd /usr/local/vpnserver/
+chmod 600 *
+chmod 700 vpnserver
+chmod 700 vpncmd
+curl -o /etc/init.d/vpnserver -L https://raw.githubusercontent.com/rezabagh/reza/master/vpnserver
+chmod 755 /etc/init.d/vpnserver
+mkdir /var/lock/subsys
+chkconfig --add vpnserver
+/etc/init.d/vpnserver start
+systemctl restart vpnserver
+systemctl status vpnserver

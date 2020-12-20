39e907e2bc47cb548cd44e602887093050f40e15
rezabagh <rezabagh@gmail.com>
2020-12-20 11:10:16 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ PORT = 443
 USERS = {"vpsi": "9cf0176721eddc8bb50878cfc1e4b707"}
 USER_MAX_TCP_CONNS = {  }
 TLS_DOMAIN = "www.cloudflare.com"
+AD_TAG = "b8ff9a4e5117a3a044d1633d042e43f9"
 MODES = { "classic": False, "secure": False, "tls": True }
 

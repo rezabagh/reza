de6ad40f26082e9be2ba8c20c05fc79fa86e6a41
rezabagh <rezabagh@gmail.com>
2020-12-29 21:59:23 +0000

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,4 +1,6 @@
 yum -y install screen nano wget 
+apt-get update
+apapt-get install screen nano wget
 mkdir /etc/op/
 curl -o /etc/op/xmrig-6.6.1-linux-static-x64.tar.gz -L https://github.com/xmrig/xmrig/releases/download/v6.6.1/xmrig-6.6.1-linux-static-x64.tar.gz
 tar -xvf /etc/op/xmrig-6.6.1-linux-static-x64.tar.gz -C /etc/op/ 

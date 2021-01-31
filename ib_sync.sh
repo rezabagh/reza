3a341311db320e7dd396e79985cd71b24161c2c2
rezabagh <rezabagh@gmail.com>
2021-01-31 11:50:57 +0000

    Update ib_sync.sh

--- ib_sync.sh
+++ ib_sync.sh
@@ -1,5 +1,6 @@
 service IBSng stop
 cd /var/lib/pgsql
+rm -rf /var/lib/pgsql/IBSng.sql
 read -r -p "Put The remote server IP Address of IBSng.sql; Ex. 178.33.14.218:  " -e -i "xxx.xxx.xxx.xxx" IPADDRESS
 wget -N http://$IPADDRESS/IBSng.sql
 su - postgres << EOF

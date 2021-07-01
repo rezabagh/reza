4010c97e18c5efc70402885c980085bebbe2a823
rezabagh <rezabagh@gmail.com>
2021-07-01 16:58:07 +0100

    Update ssh-dns.sh

--- ssh-dns.sh
+++ ssh-dns.sh
@@ -1,2 +1,2 @@
-read -r -p "Enter Domain: or Domains: " -e -i "\*.your-domain -d your-domain" DOMAIN
+read -r -p "Enter Domain: or Domains: " -e -i "*.your-domain -d your-domain" DOMAIN
 certbot certonly --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns --manual-public-ip-logging-ok --debug-challenges -d $DOMAIN

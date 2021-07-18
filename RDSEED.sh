741601296186ab01130cad2792e7c5addc3f375f
rezabagh <rezabagh@gmail.com>
2021-07-19 00:10:59 +0100

    Update RDSEED.sh

--- RDSEED.sh
+++ RDSEED.sh
@@ -1,11 +1,11 @@
 SECRET=$(openssl rand -hex 16)
 echo "This is your new Random SECRET"
 echo "$SECRET"
-sleep 30
+sleep 5
 curl -o /opt/mtprotoproxy/config.py -L https://raw.githubusercontent.com/rezabagh/reza/master/config-py
 sed -i "s/OLDSECRET/$SECRET/g" /opt/mtprotoproxy/config.py
 systemctl restart mtprotoproxy
-sleep 30
+sleep 10
 echo "Please USE this NEW MTPROTO LINK"
 systemctl status mtprotoproxy -l
 rm -rf *.*

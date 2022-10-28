bf42b41e2dd287aac1f798867df43828df68e608
rezabagh <rezabagh@gmail.com>
2022-10-28 17:52:27 +0100

    Create config-443.py

---
+++ config-443.py
@@ -0,0 +1,6 @@
+PORT = 444
+USERS = {"vpsi": "9cf0176721eddc8bb50878cfc1e4b707"}
+USER_MAX_TCP_CONNS = {  }
+TLS_DOMAIN = "www.cloudflare.com"
+AD_TAG = ""
+MODES = { "classic": False, "secure": False, "tls": True }

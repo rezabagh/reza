5222bf10149ff2adc06f558b30beae6a7f46ae07
rezabagh <rezabagh@gmail.com>
2022-12-05 20:16:01 +0000

    Update ib_backup.sh

--- ib_backup.sh
+++ ib_backup.sh
@@ -1,7 +1,7 @@
 rm -rf /var/lib/pgsql/IBSng.sql
+rm -rf /var/www/html/IBSng.sql
 su - postgres << EOF
 pg_dump IBSng > IBSng.sql
 exit
 EOF
-mkdir /var/www/html
 mv /var/lib/pgsql/IBSng.sql /var/www/html/

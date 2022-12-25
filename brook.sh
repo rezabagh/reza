240c20054e203518c44d86406bc6d9db2d27e4d2
rezabagh <rezabagh@gmail.com>
2022-12-25 15:06:23 +0000

    Update brook.sh

--- brook.sh
+++ brook.sh
@@ -10,19 +10,24 @@ rm -rf /etc/brook/brook
 curl -L https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -o /etc/brook/brook
 chmod +x /etc/brook/brook
 curl -o /etc/systemd/system/wsserver.service -L https://raw.githubusercontent.com/rezabagh/reza/master/wsserver.service
+curl -o /etc/systemd/system/wssserver.service -L https://raw.githubusercontent.com/rezabagh/reza/master/wssserver.service
 curl -o /etc/systemd/system/brook.service -L https://raw.githubusercontent.com/rezabagh/reza/master/brook.service
 curl -o /etc/systemd/system/socks5.service -L https://raw.githubusercontent.com/rezabagh/reza/master/socks5.service
 systemctl daemon-reload
 systemctl enable wsserver.service
+systemctl enable wssserver.service
 sleep 3
 systemctl stop wsserver.service
+systemctl stop wssserver.service
 systemctl stop brook.service
 systemctl stop socks5.service
 sleep 3
 systemctl restart wsserver.service
+systemctl restart wssserver.service
 systemctl restart brook.service
 systemctl restart socks5.service
 sleep 3
 systemctl status wsserver.service -l
+systemctl status wssserver.service -l
 systemctl status brook.service -l
 systemctl status socks5.service -l

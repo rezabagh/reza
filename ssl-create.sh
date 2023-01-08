0d34f4a24fc6b624c62d30964b693662b7c48a41
rezabagh <rezabagh@gmail.com>
2023-01-08 13:12:30 +0000

    Create ssl-create.sh

---
+++ ssl-create.sh
@@ -0,0 +1,4 @@
+mkdir /etc/ssl/
+read -r -p "Enter Domain: or Domains: " -e -i "www.example.com or www.example.com -d www.domain2.com -d domain3.com" DOMAIN
+~/.acme.sh/acme.sh --issue -d $DOMAIN --standalone
+~/.acme.sh/acme.sh --installcert -d $DOMAIN  --cert-file /etc/ssl/cert.pem --key-file /etc/ssl/certkey.pem --fullchain-file /etc/ssl/fullchain.pem

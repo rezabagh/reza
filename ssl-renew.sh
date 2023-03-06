af1509f8526b22913f6958af0ad178eed6f7a2ff
rezabagh <rezabagh@gmail.com>
2023-03-06 12:22:07 +0000

    Update ssl-renew.sh

--- ssl-renew.sh
+++ ssl-renew.sh
@@ -1,6 +1,6 @@
 systemctl stop httpd
 read -r -p "Enter Domain: or Domains to RENEW: " -e -i "www.example.com or www.example.com -d www.domain2.com -d domain3.com" DOMAIN
-~/.acme.sh/acme.sh --renew -d $DOMAIN --force
+~/.acme.sh/acme.sh --renew -d $DOMAIN --force --keylength 4096
 ~/.acme.sh/acme.sh --installcert -d $DOMAIN  --cert-file /etc/ssl/cert.pem --key-file /etc/ssl/certkey.pem --fullchain-file /etc/ssl/fullchain.pem
 systemctl restart httpd
 systemctl status  httpd -l
